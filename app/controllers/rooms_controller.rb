072bae456c4d8b06bff8c74709dc05244b9adbf1
Kunal Kapoor <kkapoor@ncsu.edu>
2016-09-21 10:43:06 -0400

    a little roomie

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,71 +4,91 @@ class RoomsController < ApplicationController
   # GET /rooms
   # GET /rooms.json
   def index
-    @rooms = Room.all
+    if !logged_in?
+      flash.now[:danger] = 'You are not logged in. Please login to continue.'
+    elsif !current_user.admin
+      flash.now[:danger] = "You are not authorized to view this page."
+    else
+      @rooms = Room.all
+      # Add booked field based on start and end date
+    end
   end
 
   # GET /rooms/1
   # GET /rooms/1.json
   def show
+    if !logged_in?
+      flash.now[:danger] = 'You are not logged in. Please login to continue.'
+    else
+      @room = Room.find(params[:id])
+      if current_user.admin
+        # Get library member who has booked the room
+      end
+    end
   end
 
   # GET /rooms/new
   def new
-    @room = Room.new
+    if !logged_in?
+      flash.now[:danger] = 'You are not logged in. Please login to continue.'
+    elsif !current_user.admin
+      flash.now[:danger] = 'You are not authorized to view this page.'
+    else
+      @room = Room.new
+    end
   end
 
   # GET /rooms/1/edit
   def edit
+    if !logged_in?
+      flash.now[:danger] = 'You are not logged in. Please login to continue.'
+    else
+      @room = Room.find(params[:id])
+      if !current_user.admin
+        flash.now[:danger] = "You are not authorized to view this page."
+        @room = nil
+      end
+    end
   end
 
   # POST /rooms
   # POST /rooms.json
   def create
     @room = Room.new(room_params)
-
-    respond_to do |format|
-      if @room.save
-        format.html { redirect_to @room, notice: 'Room was successfully created.' }
-        format.json { render :show, status: :created, location: @room }
-      else
-        format.html { render :new }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
-      end
+    if @room.save
+      redirect_to @room, notice: 'Room was successfully created.'
+    else
+      render :new
     end
+
   end
 
   # PATCH/PUT /rooms/1
   # PATCH/PUT /rooms/1.json
   def update
-    respond_to do |format|
-      if @room.update(room_params)
-        format.html { redirect_to @room, notice: 'Room was successfully updated.' }
-        format.json { render :show, status: :ok, location: @room }
-      else
-        format.html { render :edit }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
-      end
+    if @room.update(room_params)
+      redirect_to @room, notice: 'Room was successfully updated.'
+    else
+      render :edit
     end
+
   end
 
   # DELETE /rooms/1
   # DELETE /rooms/1.json
   def destroy
     @room.destroy
-    respond_to do |format|
-      format.html { redirect_to rooms_url, notice: 'Room was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to rooms_url, notice: 'Room was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_room
-      @room = Room.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_room
+    @room = Room.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def room_params
-      params.require(:room).permit(:number, :building, :size)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def room_params
+    params.require(:room).permit(:number, :building, :size)
+  end
 end
